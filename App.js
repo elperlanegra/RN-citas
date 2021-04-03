771269b6d8876363cdf99add7884d008844e9084
Manuel Duarte <carlosd99m@gmail.com>
2021-04-03 13:14:25 -0600

    Mostrando elementos con useState

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
 const App = () => {
+  // definir el state de citas
+  const [citas, setCitas] = useState([
+    {id: '1', paciente: 'Hook', propietario: 'Manuel', sintomas: 'No come'},
+    {id: '2', paciente: 'Redux', propietario: 'Pedro', sintomas: 'No come'},
+    {id: '2', paciente: 'Native', propietario: 'Carlos', sintomas: 'No come'},
+  ]);
+
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de citas</Text>
+
+      {citas.map(cita => (
+        <View>
+          <Text>{cita.paciente}</Text>
+        </View>
+      ))}
     </View>
   );
 };
